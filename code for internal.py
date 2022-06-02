21907471bcf97f596911cfb4fb31351b198aff0e
RyderE <86691185+RyderE@users.noreply.github.com>
2022-06-03 11:06:37 +1200

    final python with testing sheet included

---
+++ code for internal.py
@@ -0,0 +1,17 @@
+# introduction
+print("welcome to my quiz")
+print("if you would like to do the quiz on faviorite colours type 1")
+print("if you would like to do the quiz on faviorite animals type 2")
+quiz_choice = input(" ")
+
+
+# if user pick one do this
+if quiz_choice == "1":
+    print("welcome to the faviorite colours quiz")
+    print("please type 1, 2, 3 or 4 to answer the questions")
+
+
+# if user 2 picks this do this
+if quiz_choice == "2":
+    print("welcome to the favourite animals quiz")
+    print("please type 1, 2, 3 or 4 to answer the questions")
